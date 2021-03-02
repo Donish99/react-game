2a71c7dff3a6c4a550b2900e366574d2ac538b8d
Your Name <you@example.com>
2021-03-02 22:29:13 +0500

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import linkedIn from "../images/linkedin.png";
+import github from "../images/github.png";
 
 const Footer = () => {
   return (
@@ -6,17 +8,16 @@ const Footer = () => {
       style={{
         position: "absolute",
         bottom: 0,
+        right: 0,
         display: "flex",
       }}
     >
-      <a href="https://github.com/Donish99">
-        <img
-          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png"
-          alt="gitLogo"
-          height="20px"
-        />
+      <a href="https://www.linkedin.com/in/akhmadullo-nurmakhamatov-8165111b3/">
+        <img src={linkedIn} alt="LikedIn" height="20px" />
       </a>
-      |
+      <a href="https://github.com/Donish99">
+        <img src={github} alt="gitLogo" height="20px" />
+      </a>
       <a href="https://rs.school/js/">
         <img
           src="https://rs.school/images/rs_school_js.svg"
